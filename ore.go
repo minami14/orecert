de5a9caea59c9e1d5867d09d0a517fccaa302fd9
minami14 <minami-git@minami-vr.sakura.ne.jp>
2021-02-23 19:11:44 +0900

    Update ore.go

--- ore.go
+++ ore.go
@@ -11,28 +11,24 @@ import (
 
 // Generate self-signed certificate for test.
 // Don't use it for production.
-func Generate(protos []string) (*tls.Config, error) {
+func Generate() (tls.Certificate, error) {
 	key, err := rsa.GenerateKey(rand.Reader, 1024)
 	if err != nil {
-		return nil, err
+		return tls.Certificate{}, err
 	}
 
 	template := x509.Certificate{SerialNumber: big.NewInt(1)}
 	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
 	if err != nil {
-		return nil, err
+		return tls.Certificate{}, err
 	}
 
 	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
 	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
 	cert, err := tls.X509KeyPair(certPEM, keyPEM)
 	if err != nil {
-		return nil, err
+		return tls.Certificate{}, err
 	}
 
-	return &tls.Config{
-		InsecureSkipVerify: true,
-		Certificates:       []tls.Certificate{cert},
-		NextProtos:         protos,
-	}, nil
+	return cert, nil
 }
