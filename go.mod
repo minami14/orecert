f963b3b3a5d964c1fd4a05e4da93ee6854f66395
minami14 <minami-git@minami-vr.sakura.ne.jp>
2021-02-23 19:04:23 +0900

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/minami14/orecert
+
+go 1.15
---
+++ ore.go
@@ -0,0 +1,38 @@
+package orecert
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"math/big"
+)
+
+// Generate self-signed certificate for test.
+// Don't use it for production.
+func Generate(protos []string) (*tls.Config, error) {
+	key, err := rsa.GenerateKey(rand.Reader, 1024)
+	if err != nil {
+		return nil, err
+	}
+
+	template := x509.Certificate{SerialNumber: big.NewInt(1)}
+	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
+	if err != nil {
+		return nil, err
+	}
+
+	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
+	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
+	cert, err := tls.X509KeyPair(certPEM, keyPEM)
+	if err != nil {
+		return nil, err
+	}
+
+	return &tls.Config{
+		InsecureSkipVerify: true,
+		Certificates:       []tls.Certificate{cert},
+		NextProtos:         protos,
+	}, nil
+}
